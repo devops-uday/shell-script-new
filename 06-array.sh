e424531050b91b923dcffb3623fb3944b63e625e
udaykiran <yuday0555@gmail.com>
2023-10-19 00:53:35 +0530

    commiting hello world new

--- 06-array.sh
+++ 06-array.sh
@@ -3,6 +3,6 @@
 PERSON=("Uday" "Ram" "Raj")
 
 echo "first person is : ${PERSON[0]}"
-echo "first person is : ${PERSON[1]}"
-echo "first person is : ${PERSON[2]}"
-echo "first person is : ${PERSON[@]}"
+echo "second person is : ${PERSON[1]}"
+echo "third person is : ${PERSON[2]}"
+echo "all persons are : ${PERSON[@]}"
