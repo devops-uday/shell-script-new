56e3134c18594a178d2e9407d08fbd81180bead5
udaykiran <yuday0555@gmail.com>
2023-10-19 00:40:41 +0530

    commiting hello world new

--- 05-variables.sh
+++ 05-variables.sh
@@ -7,4 +7,13 @@ echo "username is $USERNAME"
 
 echo "please enter your password"
 read password
-echo " password is $password"
+echo "password is $password"
+
+echo "please enter your username"
+read -s USERNAME
+echo "username is $USERNAME"
+
+
+echo "please enter your password"
+read -s password
+echo "password is $password"
