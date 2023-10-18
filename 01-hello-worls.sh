7b8c5882cf128ed7a49b884b5368b4f74fd9fe18
udaykiran <yuday0555@gmail.com>
2023-10-18 23:54:28 +0530

    commiting hello world new

--- 01-hello-worls.sh
+++ 01-hello-worls.sh
@@ -5,6 +5,10 @@ echo "hai, am learning devops with aws"
 
 
 
-echo "Ramesh: hai, how are you Suresh?"
-echo "Suresh: am  good Ramesh, how are you Ramesh?"
-echo "Ramesh: am good Suresh."
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "$PERSON1: hai, how are you $PERSON2?"
+echo "$PERSON2: am  good $PERSON1, how are you $PERSON1?"
+echo "$PERSON1: am good $PERSON2."
