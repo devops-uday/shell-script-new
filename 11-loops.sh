95c83014c476e8a853b5f144b9ad2890add66efe
udaykiran <yuday0555@gmail.com>
2023-10-21 00:29:25 +0530

    commiting conditions

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo 1
-echo 2
-echo 3
-echo 4
-echo 5
-echo 6
+# echo 1
+# echo 2
+# echo 3
+# echo 4
+# echo 5
+# echo 6
+
+#create a loop
+
+for i in {1..100}
+do
+    echo $i
+done
