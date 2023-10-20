943e11ac3dbbb4cdc2cda4940c2a217a52881963
udaykiran <yuday0555@gmail.com>
2023-10-21 00:16:22 +0530

    commiting conditions

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,14 +4,19 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
 VALIDATE(){
 
 if [ $1 -ne 0 ]
  then
-    echo "$2 .... FAILURE"
+    echo "$2 ....$R FAILURE $N"
     exit 1
 else 
-    echo "$2 ..... SUCCESS"
+    echo "$2 .....$G SUCCESS $N"
 fi
 
 }
