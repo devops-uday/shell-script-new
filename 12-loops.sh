10d6916e1142f25cd43f0268690a92aa9e93d0d3
udaykiran <yuday0555@gmail.com>
2023-10-21 01:05:48 +0530

    commiting conditions

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $USERID -ne 0]
+then 
+    echo -e "$R ERROR: Please run the code with sudo access $N"
+    exit 1
+fi
+
 for i in $@
 do 
     yum install $i -y
