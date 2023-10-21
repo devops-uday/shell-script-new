d036b55675a887ef1ce8ef8810575468638ac379
udaykiran <yuday0555@gmail.com>
2023-10-21 22:10:42 +0530

    commiting conditions

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -11,7 +11,7 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 
-echo "script started executing at $DATE"
+echo "script started executing at $DATE" &>>$LOGFILE
 echo "$FILES_TO_DELETE"
 
 
