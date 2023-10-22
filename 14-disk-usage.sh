474d39119941a16c3f379697107c994086b6d747
udaykiran <yuday0555@gmail.com>
2023-10-22 12:11:16 +0530

    commiting

--- 14-disk-usage.sh
+++ 14-disk-usage.sh
@@ -15,4 +15,11 @@ Y="\e[33m"
 N="\e[0m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpf|Filesystem')
+DISK_USAGE_THRESHOLD=1
+
+# 'IFS= 'means INTERNAL FIELD SEPARATOR is 'space'
+while IFS= read line
+do 
+    echo "OUTPUT: $line"
+done <<< $DISK_USAGE
 
