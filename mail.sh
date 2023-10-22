21972f40b7a33ddae6052da47cb54c4f55d7b01a
udaykiran <yuday0555@gmail.com>
2023-10-22 16:15:00 +0530

    commiting

--- mail.sh
+++ mail.sh
@@ -7,11 +7,11 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$(sed -e 's/\//\\\//g' <<< $3)
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-
+echo "Escape content : $BODY"
 #echo " all args : $@"
 
 FINAL_BODY=$(sed -e 's/TEAM_NAME/DEVOPS TEAM/g' -e 's/ALERT_TYPE/High Disk Usage/g' -e "s/MEASSAGE/$BODY" template.html)
